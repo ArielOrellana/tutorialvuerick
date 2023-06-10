04dc126028fe72e81d09d31bba6df6bca8f762bf
ArielOrellana <orellanaariel1998@gmail.com>
2023-06-10 01:58:27 -0300

    primer commit

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div class="characters">
+            <div class="characters__items">
+                <h2>Characters</h2>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import { useStore } from 'vuex'
+    export default{
+        setup(){
+
+        }
+    }
+</script>
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    character: [],
+    characterFilter: [],
   },
   getters: {
   },
   mutations: {
+    setCharacters(state, payload){
+      state.character = payload
+    },
+    setCharactersFilter(state, payload){
+      state.characterFilter = payload
+    },
   },
   actions: {
+    async getCharacters({commit}){
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
